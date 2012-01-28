fd78654ce502026c527d0ec5c2358c4ad8bf511b
Chris McKnight <cmckni3@gmail.com>
2012-01-27 19:48:01 -0600

    initial commit

---
+++ WindowsGame1/WindowsGame1/Sprite.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace WindowsGame1
+{
+    class Sprite
+    {
+        //The current position of the Sprite
+        public Vector2 Position = new Vector2(0, 0);
+        
+        //The texture object used when drawing the sprite
+        private Texture2D mSpriteTexture;
+
+        //Load the texture for the sprite using the Content Pipeline
+        public void LoadContent(ContentManager theContentManager, string theAssetName)
+        {
+            mSpriteTexture = theContentManager.Load<Texture2D>(theAssetName);
+        }
+
+        //Draw the sprite to the screen
+        public void Draw(SpriteBatch theSpriteBatch)
+        {
+            theSpriteBatch.Draw(mSpriteTexture, Position, Color.White);
+        }
+    }
+}
